19c70a07492f6bbea6087b43d5a8c08b05ccfa07
geanbaila <geanbaila@gmail.com>
2021-09-29 09:59:13 -0500

    elaborando interfaces

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1709,20 +1709,15 @@
 									<!--begin::Breadcrumb-->
 									<ul class="breadcrumb breadcrumb-separatorless fw-bold fs-7 my-1">
 										<li class="breadcrumb-item text-dark">
-											<a href="#" class="btn btn-sm btn-secondary active">Cargas y encomiendas</a>
+											<a href="{{url('/')}}" class="btn btn-sm btn-secondary active">Cargas y encomiendas</a>
 										</li>
 										<li class="breadcrumb-item text-dark">
-											<a href="#" class="btn btn-sm btn-secondary">Gestión de documentos</a>
-										</li>
-										<li class="breadcrumb-item text-dark">
-											<a href="#" class="btn btn-sm btn-secondary">
+											<a href="{{url('/manifiesto')}}" class="btn btn-sm btn-secondary">
 												Manifiesto de carga
 											</a>
 										</li>
 										<li class="breadcrumb-item text-dark">
-											<a href="#" class="btn btn-sm btn-secondary">
-												Entrega de carga/documentos
-											</a>
+											<a href="#" class="btn btn-sm btn-secondary">Gestión de documentos</a>
 										</li>
 										<li class="breadcrumb-item text-dark">
 											<a href="#" class="btn btn-sm btn-secondary">
