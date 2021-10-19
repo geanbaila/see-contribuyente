4ad73abbf0da1999ecce5d62a9c8811a851b98d0
geanbaila <geanbaila@gmail.com>
2021-10-19 14:14:26 -0500

    migrate to laravel 7

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layout');
-});
-
-Route::get('/manifiesto', function () {
-    return view('layout_manifiesto');
+    return view('welcome');
 });
