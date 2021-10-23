7ce1ccc7d48318f3cf816f2d750069cfc136f5bd
geanbaila <geanbaila@gmail.com>
2021-10-23 02:49:41 -0500

    feature: print bills

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -8,6 +8,7 @@ use App\Business\Sede;
 use App\Business\Documento;
 use App\Business\Encargo;
 use MongoDB\BSON\ObjectId;
+use PDF;
 
 class SaleController extends Controller
 {
@@ -62,6 +63,7 @@ class SaleController extends Controller
             $encargo = Encargo::create($insertEncargo);
             $encargoId = $encargo['id'];
         }
+        $this->writeBill($encargoId);
         return \response()->json([ 'status' => 'OK', 'result' => ['encargoId' => $encargoId] ]);
     }
 
@@ -80,5 +82,95 @@ class SaleController extends Controller
     public function list() {
         $encargo = Encargo::all();
         return view('sale.list')->with([ 'encargo' => $encargo ]);
+    }
+
+    public function writeBill($encargoId) {
+        $encargo = Encargo::find($encargoId);
+        if ($encargo) {
+            $empresa = "M&C ENLACES S.A";
+            $direccion = "AV JAVIER PRADO ESTE NRO Sit sunt enim sunt laborum adipisicing esse officia consequat.";
+            $ruc = "RUC: 20193185060";
+            $agencia = "T.T. Atocongo";
+            $telefono = "Teléfonos: 999 999 999";
+            $transaccion = "BOLETA DE VENTA ELECTRÓNICA B007 -0005";
+            $fechaTransaccion = "08/10/2021"; 
+            $clienteRazonSocial = "Razon Social:\nCALAVERA DE MIRANDA FELICITAS";
+            $clienteDireccion = "Dirección:\nALGUNA DIRECCION";
+            $clienteDoc = "RUC/DNI: 47134373";
+            $clienteConsigna = "GEAN\nVAL JEAN";
+            
+
+            PDF::SetTitle('Hello World');
+            PDF::setPrintHeader(false);
+            PDF::setPrintFooter(false);
+            PDF::SetFont('times', 'B', 10);
+            PDF::AddPage();
+            $width = 50;
+            
+            // Color and font restoration
+            PDF::SetFillColor(255, 255, 255);
+            PDF::SetTextColor(0);
+            
+            $border = false;
+            $align_center = 'C';
+            $align_left = 'L';
+            $height = '';
+            $y = '';
+            $x = ''; // 5
+            $width = 60;
+
+            PDF::SetFont('', 'B');
+            PDF::MultiCell($width, $height, $empresa, $border, $align_center, 'LR', 'LR', $x, $y, true);
+            PDF::Ln();
+            PDF::SetFont('');
+            PDF::MultiCell($width, $height, $agencia, $border, $align_center, 1, 0, $x, $y);
+            PDF::Ln();
+            PDF::SetFont('', 'B');
+            PDF::Cell($width, $height, $ruc, 'B', 1, 'C', 0);
+
+            PDF::SetFont('');
+            PDF::MultiCell($width, $height, "Terminal:", $border, $align_left, 1, 0, $x, $y);
+            PDF::Ln();
+
+            PDF::SetFont('', 'B');
+            PDF::MultiCell($width, $height, $agencia, $border, $align_left, 1, 0, $x, $y);
+            PDF::Ln();
+
+            PDF::SetFont('');
+            PDF::Cell($width, $height, $telefono, 'B', 1, 'L', 0);
+
+            PDF::SetFont('', 'B');
+            PDF::MultiCell($width, $height, $transaccion, $border, $align_center, 1, 0, $x, $y);
+            PDF::Ln();
+
+            PDF::SetFont('');
+            PDF::Cell($width, $height, $fechaTransaccion, 'B', 1, 'L', 0);
+
+            PDF::MultiCell($width, $height, $clienteRazonSocial, $border, $align_left, 1, 0, $x, $y);
+            PDF::Ln();
+
+            PDF::MultiCell($width, $height, $clienteDireccion, $border, $align_left, 1, 0, $x, $y);
+            PDF::Ln();
+            PDF::Cell($width, $height, $clienteDoc, 'B', 1, 'L', 0);
+
+            PDF::MultiCell($width, $height, "Consigna:", $border, $align_left, 1, 0, $x, $y);
+            PDF::Ln();
+
+            PDF::SetFont('', 'B');
+            PDF::MultiCell($width, $height, $clienteConsigna, $border, $align_left, 1, 0, $x, $y);
+            PDF::Ln();
+
+            PDF::SetFont('');
+            PDF::MultiCell($width/3, $height, "Destino:", $border, $align_left, 1, 0, $x, $y);
+            PDF::SetFont('', 'B');
+            PDF::MultiCell($width/2, $height, "AREQUIPA", $border, $align_left, 1, 0, 35, $y);
+            PDF::Ln();
+            PDF::Cell($width, $height, "", 'T', 1, 'L', 0, '', 10);
+            
+
+
+            PDF::Output(public_path('hello_world.pdf'), 'F');
+            PDF::reset();
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/{encargoId}', 'SaleController@writeBill');
 
 
 Route::get('/venta', 'SaleController@list');
